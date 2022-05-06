d756af4cbf475a9808bebcc395c49e44e4d39e68
gomesfilipe <filipejvmf@gmail.com>
2022-05-05 21:46:18 -0300

    .

--- aula05/http.js
+++ aula05/http.js
@@ -1,8 +1,8 @@
-var http = require('http') // Importando módulo http
+var http = require('http') // Importando módulo http.
 
 http.createServer(function(req, res) { // Função de callback passada.
-    res.end('Hello World!') // O parâmrtro res é para enviar respostas e o req requisições
-}).listen(8081) // Porta 8081
+    res.end('Hello World!') // O parâmetro res é para enviar respostas e o req requisições.
+}).listen(8081) // Porta 8081.
 
 console.log('The server is running!')
 
